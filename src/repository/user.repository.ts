34d70d354aa9f69e2c923d17bbe5637b3b5460ce
Nitiwat-owen <77772598+Nitiwat-owen@users.noreply.github.com>
2023-09-14 09:36:43 +0700

    [SPGO-13] Create login function
    * apply yarn instead npm
    
    * create account.proto
    
    * change package name
    
    * generate auth grpc interface
    
    * create auth service
    
    * create login service
    
    * create access token and refresh token strategy
    
    * update user table
    
    * update refresh token when login success

---
+++ src/repository/user.repository.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@nestjs/common';
+import { User } from '@prisma/client';
+import { PrismaService } from '../prisma/service/prisma.service';
+
+@Injectable()
+export class UserRepository {
+  constructor(private db: PrismaService) {}
+
+  async getUserByEmail(email: string): Promise<User> {
+    return this.db.user.findUniqueOrThrow({ where: { email: email } });
+  }
+
+  async updateRefreshToken(
+    userId: string,
+    refreshToken: string,
+  ): Promise<User> {
+    return this.db.user.update({
+      where: { id: userId },
+      data: { refreshToken: refreshToken },
+    });
+  }
+}

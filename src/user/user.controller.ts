c2a378aedace46a929d87f606805cc664aa52650
NitiwatOwen <nitiwat3478@gmail.com>
2023-09-20 21:56:16 +0700

    init user controller and user service

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Patch } from '@nestjs/common';
+import { UserService } from './user.service';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private userService: UserService) {}
+
+  @Get()
+  findAllUsers() {
+    return null;
+  }
+
+  @Patch('ban/:userId')
+  banUser() {
+    return null;
+  }
+
+  @Patch('unban/:userId')
+  unbanUser() {
+    return null;
+  }
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { UserRepository } from '../repository/user.repository';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(private userRepo: UserRepository) {}
+
+  findAllUsers() {
+    return null;
+  }
+
+  banUser() {
+    return null;
+  }
+
+  unbanUser() {
+    return null;
+  }
+}

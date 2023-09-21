34f81f8c191b2c608b21e7f3a9707fd701d238f6
KnotPP <88419066+KnotPP@users.noreply.github.com>
2023-09-21 12:29:54 +0700

    update controller spec

--- src/user/user.controller.spec.ts
+++ src/user/user.controller.spec.ts
@@ -3,10 +3,16 @@ import { UserController } from './user.controller';
 
 describe('UserController', () => {
   let controller: UserController;
+  const mockUserService = {
+    findUserById: jest.fn(),
+    update: jest.fn(),
+    exclude: jest.fn(),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UserController],
+      providers: [{ provide: 'UserService', useValue: mockUserService }],
     }).compile();
 
     controller = module.get<UserController>(UserController);

9d0474afc0f3d448ecb8b65329170125560458b6
ANAN DEESAKUL <62237588+Arnan-Dee@users.noreply.github.com>
2023-10-23 12:27:44 +0700

    Implement RabbitMQ (#16)
    * Implement RabbitMQ
    
    * Fix test suit

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Inject, Module } from '@nestjs/common';
 import { PrismaModule } from '../prisma/prisma.module';
 import { UserRepository } from '../repository/user.repository';
 import { AuthService } from './auth.service';
@@ -7,9 +7,20 @@ import { JwtModule, JwtService } from '@nestjs/jwt';
 import { AccessTokenStrategy } from './strategies/accessToken.strategy';
 import { ConfigService } from '@nestjs/config';
 import { BlacklistRepository } from 'src/repository/blacklist.repository';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
-  imports: [PrismaModule, JwtModule.register({})],
+  imports: [PrismaModule, JwtModule.register({}), ClientsModule.register([{
+    name: 'EMAIL_SERVICE',
+    transport: Transport.RMQ,
+    options: {
+      urls: ['amqp://localhost:5672'],
+      queue: 'email_queue',
+      queueOptions: {
+        durable: false
+      }
+    }
+  }])],
   providers: [AuthService, UserRepository, AccessTokenStrategy, ConfigService, BlacklistRepository],
   controllers: [AuthController],
 })

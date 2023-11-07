02018ade84720b9e5dcb75ec81ecbd6a75ec43c6
NitiwatOwen <nitiwat3478@gmail.com>
2023-11-07 08:35:21 +0700

    upload profile image when register

--- src/file/file.service.ts
+++ src/file/file.service.ts
@@ -1,4 +1,29 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable, Logger } from '@nestjs/common';
+import { ClientGrpc } from '@nestjs/microservices';
+import { catchError, firstValueFrom } from 'rxjs';
+import {
+  FileServiceClient,
+  UploadFileRequest,
+  UploadFileResponse,
+} from './file.pb';
 
 @Injectable()
-export class FileService {}
+export class FileService {
+  private fileClient: FileServiceClient;
+  private readonly logger = new Logger(FileService.name);
+
+  constructor(@Inject('FILE_PACKAGE') private client: ClientGrpc) {
+    this.fileClient = this.client.getService<FileServiceClient>('FileService');
+  }
+
+  async uploadFile(request: UploadFileRequest): Promise<UploadFileResponse> {
+    return await firstValueFrom(
+      this.fileClient.uploadFile(request).pipe(
+        catchError((error) => {
+          this.logger.error(error);
+          throw error;
+        }),
+      ),
+    );
+  }
+}
